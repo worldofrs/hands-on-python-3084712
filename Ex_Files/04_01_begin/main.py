5780982af0971b85bb2134914be1bfb2e5183657
rs555 <84246573+worldofrs@users.noreply.github.com>
2022-12-26 20:43:43 +0000

    new code algorithms

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,6 +1,9 @@
-import requests
+import  requests
 
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
 last_twenty_years = response.json()[1][:20]
+
+for year in last_twenty_years:
+    display_width = year[value]
